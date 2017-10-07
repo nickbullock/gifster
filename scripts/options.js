944181c0a1fab5c39018e13189c053283d5d8ba0
nickbullock <balakirevnikita@yandex.ru>
2017-10-07 15:17:20 +0300

    options

--- scripts/options.js
+++ scripts/options.js
@@ -3,37 +3,42 @@ import "./../style/options.css";
 
 document.addEventListener("DOMContentLoaded",
     function OptionsController() {
-        let options = {};
+        const options = {};
         const defaultOptions = {
-            duration: 5,
-            fps: 15,
-            width: 858,
-            height: 480,
-            quality: 10
+            duration: "5",
+            fps: "15",
+            resolution: "2",
+            quality: "10"
         };
 
         const DURATION = "input#duration";
-        const FRAME_RATE = "input#fps";
+        const FPS = "input#fps";
         const RESOLUTION = "input#resolution";
         const QUALITY = "input#quality";
+        const SAVE = "a.save-btn";
 
         chrome.runtime.sendMessage({optionsInit: true});
 
-        document.querySelector(DURATION).addEventListener("change", changeDurationValue);
-        document.querySelector(FRAME_RATE).addEventListener("change", changeFrameRateValue);
-        document.querySelector(RESOLUTION).addEventListener("change", changeResolutionValue);
-        document.querySelector(QUALITY).addEventListener("change", changeQualityValue);
+        document.querySelector(DURATION).addEventListener("change", getValue.bind(null, "duration"));
+        document.querySelector(FPS).addEventListener("change", getValue.bind(null, "fps"));
+        document.querySelector(RESOLUTION).addEventListener("change", getValue.bind(null, "resolution"));
+        document.querySelector(QUALITY).addEventListener("change", getValue.bind(null, "quality"));
+        document.querySelector(SAVE).addEventListener("click", save);
 
-        function changeDurationValue (event) {
+        function getValue (key, event) {
             options[event.target.id] = event.target.value;
-            document.querySelector(".settings__block_duration > .block__title > .block__value").innerHTML = event.target.value + "s";
+            renderValue(key, event.target.value)
         }
-        function changeFrameRateValue (event) {
-            options[event.target.id] = event.target.value;
-            document.querySelector(".settings__block_fps > .block__title > .block__value").innerHTML = event.target.value + "fps";
+        function setValue(key, value) {
+            document.querySelector(`input#${key}`).value = value;
+            renderValue(key, value)
         }
-        function changeResolutionValue (event) {
-            switch(event.target.value){
+        function renderValue(key, value) {
+            document.querySelector(`.settings__block_${key} > .block__title > .block__value`).innerHTML = value;
+        }
+
+        function resolutionResolver(resolution) {
+            switch(resolution) {
                 case "0":
                     options.width = 480;
                     options.height = 360;
@@ -51,43 +56,53 @@ document.addEventListener("DOMContentLoaded",
                     options.height = 1080;
                     break;
             }
-            document.querySelector(".settings__block_resolution > .block__title > .block__value").innerHTML = `${options.height}p`;
-        }
-        function changeQualityValue (event) {
-            options[event.target.id] = event.target.value;
-            document.querySelector(".settings__block_quality > .block__title > .block__value").innerHTML = event.target.value;
         }
 
-        function init(options, isFirstInit) {
+        function init(optionsInit, isFirstInit) {
             if(isFirstInit){
-                chrome.storage.set("gifsterOptions", defaultOptions);
+                chrome.storage.sync.set({gifsterOptions: defaultOptions});
+                optionsInit = defaultOptions;
             }
 
-            changeDurationValue({target:{value: defaultOptions.duration}});
-            changeFrameRateValue({target:{value: defaultOptions.fps}});
-            changeQualityValue({target:{value: defaultOptions.quality}});
-            changeResolutionValue({target:{value: "1"}});
+            console.log("[OptionsController.init] init options", optionsInit);
+
+            Object.assign(options, optionsInit);
+
+            setValue("duration", options.duration);
+            setValue("resolution", options.resolution);
+            setValue("fps", options.fps);
+            setValue("quality", options.quality);
         }
 
         function save() {
-            console.log("save", options);
+            const optionsInit = Object.assign({}, options);
+            resolutionResolver(optionsInit.resolution);
 
-            chrome.storage.set("gifsterOptions", options);
+            console.log("[OptionsController.save] saving options", optionsInit);
+
+            chrome.storage.sync.set(
+                {gifsterOptions: optionsInit},
+                () => {
+                    chrome.notifications.create({
+                        type: "basic",
+                        iconUrl: chrome.extension.getURL("icon128.png"),
+                        title: "Options saved",
+                        message: "Gifster saved your options succesfully :)"
+                    });
+                }
+            );
         }
 
         chrome.storage.sync.get(
             "gifsterOptions",
             (options) => {
-                if(options){
-                    init(options, false);
+                if(options && options.gifsterOptions && Object.keys(options.gifsterOptions).length > 0){
+                    init(options.gifsterOptions, false);
                 }
                 else{
                     init(defaultOptions, true)
                 }
             }
         );
-
-
-
     }
 );
